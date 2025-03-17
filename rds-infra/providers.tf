b9e2c36511e8486711ff279e3ef5054b0b346bde
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-03-17 13:52:45 -0300

    removed rds config

--- rds-infra/providers.tf
+++ rds-infra/providers.tf
@@ -5,14 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
-  backend "s3" {
-    bucket         = "meu-bucket-terraform-state"
-    key            = "rds/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
+#   backend "s3" {
+#     bucket         = "meu-bucket-terraform-state"
+#     key            = "rds/terraform.tfstate"
+#     region         = "us-east-1"
+#     encrypt        = true
+#     dynamodb_table = "terraform-lock"
+#   }
+ }
 
 provider "aws" {
   region = var.aws_region
