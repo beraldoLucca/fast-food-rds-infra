9b1a8e27d5dadaf9bd3c20292006ca2a0f573286
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-03-18 00:32:23 -0300

    change db name and pwd

--- rds-infra/main.tf
+++ rds-infra/main.tf
@@ -8,7 +8,6 @@ resource "aws_db_instance" "rds" {
   db_name              = var.db_name
   username            = var.db_username
   password            = var.db_password
-  parameter_group_name = "default.${var.db_engine}"
   publicly_accessible  = false
   skip_final_snapshot  = true
 
