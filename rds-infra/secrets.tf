ff62cd74e1bd19f23073c69c11b961b46dc73d7e
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-03-18 00:10:52 -0300

    update secret manager version

--- rds-infra/secrets.tf
+++ rds-infra/secrets.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "rds_secret" {
-  name = "rds-credentials-2"
+  name = "rds-credentials-3"
 }
 
 resource "aws_secretsmanager_secret_version" "rds_secret_version" {
