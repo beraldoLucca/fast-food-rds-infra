94ea6e84851dbd1508ea4abf9f35301de1207bb8
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-03-18 00:14:30 -0300

    change db name and pwd

--- rds-infra/secrets.tf
+++ rds-infra/secrets.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "rds_secret" {
-  name = "rds-credentials-3"
+  name = "rds-credentials-4"
 }
 
 resource "aws_secretsmanager_secret_version" "rds_secret_version" {
