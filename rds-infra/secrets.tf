8db3f23b601e49f19f9c4ff8a5b629ca9fed765f
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-03-19 20:20:20 -0300

    update vars

--- rds-infra/secrets.tf
+++ rds-infra/secrets.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "rds_secret" {
-  name = "rds-credentials-11"
+  name = "rds-credentials-12"
 }
 
 resource "aws_secretsmanager_secret_version" "rds_secret_version" {
