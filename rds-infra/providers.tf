1f1293e3235e049ba64c43e390d1a6feb63d24be
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-03-21 00:53:21 -0300

    update secret version

--- rds-infra/providers.tf
+++ rds-infra/providers.tf
@@ -5,13 +5,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-#   backend "s3" {
-#     bucket         = "meu-bucket-terraform-state"
-#     key            = "rds/terraform.tfstate"
-#     region         = "us-east-1"
-#     encrypt        = true
-#     dynamodb_table = "terraform-lock"
-#   }
  }
 
 provider "aws" {
